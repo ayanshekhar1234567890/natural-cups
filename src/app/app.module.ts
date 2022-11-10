699fa39967b739a973b0e0c3804ea665659b888e
= <=>
2022-11-10 21:39:25 +0530

    Some more changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainComponent,
+    ProductsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
