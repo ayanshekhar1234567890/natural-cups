8edf41f9d96089d29c041cab9ea1cf827b1e319c
= <=>
2022-11-22 11:19:03 +0530

    Basic Modelling

---
+++ src/app/products/Cup.ts
@@ -0,0 +1,7 @@
+export class Cup {
+    name!: string
+    desc!: string
+    price!: string
+    imageLink!: string
+    buyLink!: string
+}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cup } from './Cup';
 
 @Component({
   selector: 'app-products',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  cups!: Cup[]
+
+  constructor() {
+    this.cups = []
+  }
 
   ngOnInit(): void {
   }
