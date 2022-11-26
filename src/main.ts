9504c8ed62f2c8ed6a3d55abb363883449f1cf86
= <=>
2022-11-26 15:31:43 +0530

    Added Firebase

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,26 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3E_SynU-fuij6Svw316BZBpvemahqfNw",
+  authDomain: "naturalcps-15ca3.firebaseapp.com",
+  projectId: "naturalcps-15ca3",
+  storageBucket: "naturalcps-15ca3.appspot.com",
+  messagingSenderId: "184509443626",
+  appId: "1:184509443626:web:6615b014c983201b951d53",
+  measurementId: "G-G42Z5L5SY9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
